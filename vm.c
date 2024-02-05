23ec0aa647c9a0ee861614f1d872783afb7855d4
changzheng <2950448386@qq.com>
2024-02-05 10:27:55 +0800

    add stack in vm

--- vm.c
+++ vm.c
@@ -1,9 +1,17 @@
 ﻿#include <stdio.h>
+#include "common.h"
 #include "vm.h"
+#include "debug.h"
 
 VM vm;
 
+static void resetStack() {
+    // a pointer to zero position
+    vm.stackTop = vm.stack;
+}
+
 void initVM() {
+    resetStack();
 }
 
 void freeVM() {
@@ -15,15 +23,27 @@ static InterpretResult run() {
 #define READ_CONSTANT() (vm.chunk->constants.values[READ_BYTE()])
 
     for (;;) {
+#ifdef DEBUG_TRACE_EXECUTION
+        printf("stack:     ");
+        for (Value *slot = vm.stack; slot < vm.stackTop; slot++) {
+            printf("[ ");
+            printValue(*slot);
+            printf(" ]");
+        }
+        printf("\n");
+        disassembleInstruction(vm.chunk,
+                               (int) (vm.ip - vm.chunk->code));
+#endif
         uint8_t instruction;
         switch (instruction = READ_BYTE()) {
             case OP_CONSTANT: {
                 Value constant = READ_CONSTANT();
-                printValue(constant);
-                printf("\n");
+                push(constant);
                 break;
             }
             case OP_RETURN: {
+                printValue(pop());
+                printf("\n");
                 return INTERPRET_OK;
             }
         }
@@ -37,4 +57,14 @@ InterpretResult interpret(Chunk *chunk) {
     vm.chunk = chunk;
     vm.ip = vm.chunk->code;
     return run();
+}
+
+void push(Value value) {
+    *vm.stackTop = value;
+    vm.stackTop++;
+}
+
+Value pop() {
+    vm.stackTop--;
+    return *vm.stackTop;
 }
--- vm.h
+++ vm.h
@@ -4,10 +4,16 @@
 #include "chunk.h"
 #include "common.h"
 
+#define STACK_MAX 256
+
 typedef struct {
     Chunk *chunk;
     //  always points to the next instruction, not the one currently being handled.
     uint8_t *ip;
+    // stack to do calculate
+    Value stack[STACK_MAX];
+    // point to where the next value to be pushed will go
+    Value *stackTop;
 } VM;
 
 typedef enum {
@@ -21,6 +27,10 @@ void initVM();
 void freeVM();
 
 InterpretResult interpret(Chunk *chunk);
+
+void push(Value value);
+
+Value pop();
 
 
 #endif

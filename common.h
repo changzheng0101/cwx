2bf1934bf8c1309816c187d10e8debb9645bc835
changzheng <2950448386@qq.com>
2024-02-29 10:48:38 +0800

    expression

--- common.h
+++ common.h
@@ -8,7 +8,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-
+#define DEBUG_PRINT_CODE
 #define DEBUG_TRACE_EXECUTION
 
 #endif
--- compiler.h
+++ compiler.h
@@ -1,6 +1,8 @@
 ﻿#ifndef CWX_COMPILER_H
 #define CWX_COMPILER_H
 
-void compile(const char *source);
+#include "vm.h"
+
+bool  compile(const char *source, Chunk *chunk);
 
 #endif //CWX_COMPILER_H
